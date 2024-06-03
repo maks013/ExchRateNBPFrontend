2353b6e800b0286780698ecee53cca7786034668
Maksymilian Ulanecki <maks.u@vp.pl>
2024-06-03 19:49:16 +0200

    added getting current currency value

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,18 +1,44 @@
 import { Component } from '@angular/core';
-import {FooterComponent} from "../footer/footer.component";
-import {NavbarComponent} from "../navbar/navbar.component";
+import { FooterComponent } from "../footer/footer.component";
+import { NavbarComponent } from "../navbar/navbar.component";
+import { NgForOf } from "@angular/common";
+import { ExchangeRateService } from '../service/exchange-rate.service';
+import {FormsModule} from "@angular/forms"; // Ensure this path is correct
 
 @Component({
   selector: 'app-exchange',
   standalone: true,
   imports: [
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    NgForOf,
+    FormsModule
   ],
   templateUrl: './exchange.component.html',
-  styleUrl: './exchange.component.css'
+  styleUrls: ['./exchange.component.css']
 })
 export class ExchangeComponent {
-  exchange() {
+  currencies: string[] = [
+    'THB', 'USD', 'AUD', 'HKD', 'CAD', 'NZD', 'SGD',
+    'EUR', 'HUF', 'CHF', 'GBP', 'UAH', 'JPY', 'CZK',
+    'DKK', 'ISK', 'NOK', 'SEK', 'RON', 'BGN', 'TRY',
+    'ILS', 'CLP', 'PHP', 'MXN', 'ZAR', 'BRL', 'MYR',
+    'IDR', 'INR', 'KRW', 'CNY', 'XDR'
+  ];
+  selectedCurrency: string = 'USD';
+  name: string = '';
+  currentRate: number | null = null;
+
+  constructor(private exchangeRateService: ExchangeRateService) {}
+
+  checkRate() {
+    const requestData = {
+      currency: this.selectedCurrency,
+      name: this.name
+    };
+
+    this.exchangeRateService.getCurrentCurrencyValue(requestData).subscribe(response => {
+      this.currentRate = response.value;
+    });
   }
 }
--- src/app/service/exchange-rate.service.ts
+++ src/app/service/exchange-rate.service.ts
@@ -14,10 +14,15 @@ interface Request {
 })
 export class ExchangeRateService {
   private requestsUrl = 'http://localhost:8080/currencies/requests';
+  private currencyValueUrl = 'http://localhost:8080/currencies/get-current-currency-value-command'; // Corrected URL
 
   constructor(private http: HttpClient) {}
 
   getRequests(): Observable<Request[]> {
     return this.http.get<Request[]>(this.requestsUrl);
+  }
+
+  getCurrentCurrencyValue(data: { currency: string; name: string }): Observable<any> {
+    return this.http.post<any>(this.currencyValueUrl, data);
   }
 }
