4e09dc3738bff825cc94a994e205db6e699a8d2f
Maksymilian Ulanecki <maks.u@vp.pl>
2024-06-03 16:23:46 +0200

    created exchange component

--- src/app/exchange/exchange.component.ts
+++ src/app/exchange/exchange.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import {FooterComponent} from "../footer/footer.component";
+import {NavbarComponent} from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-exchange',
   standalone: true,
-  imports: [],
+  imports: [
+    FooterComponent,
+    NavbarComponent
+  ],
   templateUrl: './exchange.component.html',
   styleUrl: './exchange.component.css'
 })
 export class ExchangeComponent {
-
+  exchange() {
+  }
 }
